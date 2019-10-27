66dedaef7e9e8b5e2994f0a3aaf62454c79b2a6f
duwoleyouyou <1243809347@qq.com>
2019-10-27 14:55:45 +0800

    菜单样式修改

--- MX/src/view/main/main.vue
+++ MX/src/view/main/main.vue
@@ -1,15 +1,32 @@
 <template>
   <Layout style="height: 100%" class="main">
-    <Sider style="overflow: auto" hide-trigger collapsible :width="256" :collapsed-width="64" v-model="collapsed">
-      <side-menu accordion :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage"
-                 :menu-list="menuList">
-        <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
-        <div class="logo-con">
-          <img v-show="!collapsed" :src="maxLogo" key="max-logo" width="312" height="85"/>
-          <img v-show="collapsed" :src="minLogo" key="min-logo"/>
+    <Header>
+      <Menu mode="horizontal" theme="dark" active-name="1">
+        <div class="layout-logo"></div>
+        <div class="layout-nav" style="display: flex;flex-wrap: wrap;justify-content: space-between">
+          <MenuItem :name="item.name" v-for="(item,index) in menuList" @click.native="handleClick(item)">
+            <div style="height: 20px;text-align: center">
+              <Icon :type="item.icon"></Icon>
+            </div>
+            <div>{{ showTitleInside(item) }}</div>
+          </MenuItem>
         </div>
-      </side-menu>
-    </Sider>
+      </Menu>
+
+      <!--左侧通栏-->
+      <!--<Sider style="overflow: auto" hide-trigger collapsible :width="256" :collapsed-width="64" v-model="collapsed">-->
+        <!--<side-menu accordion :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage"-->
+                   <!--:menu-list="menuList">-->
+          <!--&lt;!&ndash; 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 &ndash;&gt;-->
+          <!--<div class="logo-con">-->
+            <!--<img v-show="!collapsed" :src="maxLogo" key="max-logo" width="312" height="85"/>-->
+            <!--<img v-show="collapsed" :src="minLogo" key="min-logo"/>-->
+          <!--</div>-->
+        <!--</side-menu>-->
+      <!--</Sider>-->
+    </Header>
+
+
     <Layout class="box_col">
       <div class="header-con">
         <header-bar :collapsed="collapsed" @on-coll-change="handleCollapsedChange">
@@ -20,9 +37,10 @@
       </div>
       <div class="box_col_100">
         <div class="box_col">
-          <div class="tag-nav-wrapper">
-            <tags-nav :value="$route" @input="handleClick" :list="tagNavList" @on-close="handleCloseTag"/>
-          </div>
+          <!--<div class="tag-nav-wrapper">-->
+            <!--&lt;!&ndash;<tags-nav :value="$route" @input="handleClick" :list="tagNavList" @on-close="handleCloseTag"/>&ndash;&gt;-->
+            <!--<tags-nav :value="$route" @input="handleClick" :list="menuList" @on-close="handleCloseTag"/>-->
+          <!--</div>-->
           <Content class="box_col_100 content-wrapper2">
             <keep-alive :include="cacheList" style="height: 100%">
               <!--<Card style="height: 100%" :style="{height:AF.getPageHeight()-68-40-36+'px'}">-->
@@ -40,6 +58,7 @@
 <script>
   import moment from 'moment'
 
+  import {showTitle} from '@/libs/util'
   import SideMenu from './components/side-menu'
   import HeaderBar from './components/header-bar'
   import TagsNav from './components/tags-nav'
@@ -125,6 +144,9 @@
       handleCollapsedChange(state) {
         this.collapsed = state
       },
+      showTitleInside (item) {
+        return showTitle(item, this)
+      },
       handleCloseTag(res, type, name) {
           //2019年10月14日。关闭异常界面，清理缓存值
           if (name == "Student-exception"){
@@ -179,3 +201,21 @@
     }
   }
 </script>
+
+<style scoped>
+  .ivu-menu-dark{
+    background: rgb(81, 90, 110);
+  }
+
+  .ivu-layout-header{
+    padding: 0;
+  }
+
+  .ivu-menu-horizontal{
+    height: auto;
+  }
+
+  .ivu-menu-horizontal .ivu-menu-item, .ivu-menu-horizontal .ivu-menu-submenu{
+    padding: 0 5px;
+  }
+</style>
