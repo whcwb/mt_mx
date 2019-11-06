8a2e848650c8ca4f93b4fa3122346c48aa62d3a2
duwoleyouyou <1243809347@qq.com>
2019-11-06 14:53:46 +0800

    ..

--- MX/src/view/charge/jlcz/mxb.vue
+++ MX/src/view/charge/jlcz/mxb.vue
@@ -1,36 +1,36 @@
 <template>
   <div>
-  <Modal
-    v-model="isMxb"
-    :title="itemObj.jlXm+' '+itemObj.cardNo+' 明细表'"
-    :closable='false'
-    :mask-closable='false'
-    @on-ok="ok">
-    <Table stripe
-           :height="AF.getPageHeight()-250"
-           :columns="columns2"
-           :data="dataList">
-    </Table>
-    <div style="text-align: right;padding: 6px 0">
-      <Page :total=totalS
-            :current=param.pageNum
-            :page-size=param.pageSize
-            :page-size-opts=[8,10,20,30,40,50]
-            show-total
-            show-elevator
-            show-sizer
-            placement='top'
-            @on-page-size-change='(n)=>{pageSizeChange(n)}'
-            @on-change='(n)=>{pageChange(n)}'>
-      </Page>
-    </div>
-    <div slot="footer">
-      <Button @click="ok">
-        关闭
-      </Button>
-    </div>
-  </Modal>
-  <component :is="componentName"  :hisPrintMess="hisPrintMess"></component>
+    <Modal
+      v-model="isMxb"
+      :title="itemObj.jlXm+' '+itemObj.cardNo+' 明细表'"
+      :closable='false'
+      :mask-closable='false'
+      @on-ok="ok">
+      <Table stripe
+             :height="AF.getPageHeight()-250"
+             :columns="columns2"
+             :data="dataList">
+      </Table>
+      <div style="text-align: right;padding: 6px 0">
+        <Page :total=totalS
+              :current=param.pageNum
+              :page-size=param.pageSize
+              :page-size-opts=[8,10,20,30,40,50]
+              show-total
+              show-elevator
+              show-sizer
+              placement='top'
+              @on-page-size-change='(n)=>{pageSizeChange(n)}'
+              @on-change='(n)=>{pageChange(n)}'>
+        </Page>
+      </div>
+      <div slot="footer">
+        <Button @click="ok">
+          关闭
+        </Button>
+      </div>
+    </Modal>
+    <component :is="componentName" :hisPrintMess="hisPrintMess"></component>
   </div>
 </template>
 
@@ -38,7 +38,7 @@
   import printSignUp from '../../lcsf/comp/printSignUp'
 
   export default {
-      components: {printSignUp},
+    components: {printSignUp},
     name: "mxb",
     props: {
       itemObj: {
@@ -53,105 +53,128 @@
     data() {
       return {
         mx: false,
-          hisPrintMess:{},
-          componentName:'',
-        totalS:0,
+        hisPrintMess: {},
+        componentName: '',
+        totalS: 0,
         param: {
           pageNum: 1,
           pageSize: 8,
-          id:''
+          id: ''
         },
         dataList: [],
         columns2: [
-          {title: '#', type: 'index', width: 60},
+          {title: '#', type: 'index', width: 90},
           {
             title: '流水时间',
-            key: 'cjsj',
             align: 'center',
-
+            render: (h, p) => {
+              return h('div', p.row.cjsj.substring(0, 16));
+            },
           },
           {
             title: '类型',
             align: 'center',
             render: (h, p) => {
-              let type=''
+              let type = ''
               switch (p.row.type) {
-                case '00':type='开放日预存'
-                      break;
-                case '10':type='卡片预存'
+                case '00':
+                  type = '预存(开放日)'
                   break;
-                case '20':type='卡片抵扣'
+                case '10':
+                  type = '预存(充值卡)'
                   break;
-                case '30':type='开放日抵扣'
+                case '20':
+                  type = '抵扣(充值卡)'
+                  break;
+                case '30':
+                  type = '抵扣(开放日)'
                   break;
               }
               return h('div', type);
             },
             filters: [
               {
-                label: '充值卡预存',
+                label: '预存(充值卡)',
                 value: '10'
               },
               {
-                label: '充值卡抵扣',
+                label: '抵扣(充值卡)',
                 value: '20'
               },
               {
-                label: '开放日预存',
+                label: '预存(开放日)',
                 value: '00'
               },
               {
-                label: '开放日抵扣',
+                label: '抵扣(开放日)',
                 value: '30'
               }
             ],
             filterMultiple: false,
-            filterMethod (value, row) {
+            filterMethod(value, row) {
               // if (value === 1) {
               //   return row.age > 25;
               // } else if (value === 2) {
               //   return row.age < 25;
               // }
               switch (value) {
-                case '00':return row.type=='00'
+                case '00':
+                  return row.type == '00'
                   break;
-                case '10':return row.type=='10'
+                case '10':
+                  return row.type == '10'
                   break;
-                case '20':return row.type=='20'
+                case '20':
+                  return row.type == '20'
                   break;
-                case '30':return row.type=='30'
+                case '30':
+                  return row.type == '30'
                   break;
               }
 
             }
           },
           {
-            title: '充值金额',
-            key: 'je',
+            title: '预存金额',
             align: 'center',
-
+            render: (h, p) => {
+              if (p.row.type == '00' || p.row.type == '10')
+                return h('div', p.row.je + '元');
+              else return h('div', '/');
+            },
+          },
+          {
+            title: '抵扣金额',
+            align: 'center',
+            render: (h, p) => {
+              if (p.row.type == '20' || p.row.type == '30')
+                return h('div', p.row.je + '元');
+              else return h('div', '/');
+            },
           },
           {
             title: '实付金额',
-            key: 'sfje',
             align: 'center',
+            render: (h, p) => {
+              return h('div', p.row.sfje + '元');
+            },
           },
           {
             title: '余额',
             align: 'center',
             render: (h, p) => {
-              let type=''
-              switch (p.row.type) {
-                case '00':type='开放日余额:'+p.row.czhje
-                  break;
-                case '30':type='开放日余额:'+p.row.czhje
-                  break;
-                case '10':type='充值卡余额:'+p.row.czhje
-                  break;
-                case '20':type='充值卡余额:'+p.row.czhje
-                  break;
-              }
-              return h('div', type);
+              let type = ''
+              // switch (p.row.type) {
+              //   case '00':type='开放日余额:'+p.row.czhje+'元'
+              //     break;
+              //   case '30':type='开放日余额:'+p.row.czhje+'元'
+              //     break;
+              //   case '10':type='充值卡余额:'+p.row.czhje+'元'
+              //     break;
+              //   case '20':type='充值卡余额:'+p.row.czhje+'元'
+              //     break;
+              // }
+              return h('div', p.row.czhje + '元');
             }
           },
           {
@@ -159,32 +182,32 @@
             key: 'bz',
             align: 'center',
           },
-            {
-                title: '操作',
-                align: 'center',
-                render:(h,p)=>{
-                    if(p.row.type == '10'){
-                        return h('Button', {
-                            props: {
-                                type: 'info',
-                                size: 'small'
-                            },
-                            style: {
-                                borderRadius: '15px'
-                            },
-                            on:{
-                                click:()=>{
-                                    this.hisPrintMess = p.row
-                                    this.componentName = 'printSignUp'
-                                }
-                            }
-                        }, '打印')
-                    }else {
-                        return ''
+          {
+            title: '操作',
+            align: 'center',
+            render: (h, p) => {
+              if (p.row.type == '10') {
+                return h('Button', {
+                  props: {
+                    type: 'info',
+                    size: 'small'
+                  },
+                  style: {
+                    borderRadius: '15px'
+                  },
+                  on: {
+                    click: () => {
+                      this.hisPrintMess = p.row
+                      this.componentName = 'printSignUp'
                     }
+                  }
+                }, '打印')
+              } else {
+                return ''
+              }
 
-                }
             }
+          }
         ],
       }
     },
@@ -197,7 +220,7 @@
     },
     methods: {
       getData() {
-        this.param.id=this.itemObj.id
+        this.param.id = this.itemObj.id
         this.$http.post('/api/lcwxjl/czmx', this.param).then(res => {
           if (res.code == 200) {
             this.totalS = res.page.total
@@ -211,11 +234,11 @@
         }).catch(err => {
         })
       },
-      pageChange(val){
+      pageChange(val) {
         this.param.pageNum = val
         this.getData();
       },
-      pageSizeChange(val){
+      pageSizeChange(val) {
         this.param.pageSize = val
         this.getData();
       },
