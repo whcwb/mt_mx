16f6c2b273cb6064963b08e65c04f27ee9e43cb2
quan <1538452228@qq.com>
2019-12-03 17:12:52 +0800

    1

--- MX/src/axios/api.js
+++ MX/src/axios/api.js
@@ -1,6 +1,6 @@
 // let ajaxUrl = 'http://127.0.0.1:8080';
 // let ajaxUrl = 'http://192.168.1.156:8080';
-let ajaxUrl ='http://192.168.0.102:8080';//宋
+let ajaxUrl ='http://192.168.0.105:8080';//宋
 // let ajaxUrl ='http://192.168.0.104:8080';//张
 // let ajaxUrl ='http://192.168.123.69:8080';//宋
 // let ajaxUrl ='http://172.16.0.33:8080';//宋
