af8745503e62d5d029feae27df77ffc3ed4edeb3
slu <384920400@qq.com>
2019-12-09 15:02:11 +0800

    update

--- biz/src/main/java/com/ldz/biz/service/impl/BizLcFdServiceImpl.java
+++ biz/src/main/java/com/ldz/biz/service/impl/BizLcFdServiceImpl.java
@@ -23,7 +23,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tk.mybatis.mapper.common.Mapper;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
